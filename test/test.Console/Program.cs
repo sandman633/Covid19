a8981d1c97818c400567eaff41bbf3a1d66f3769
sandman633 <sad_is_bad@outlook.com>
2022-07-19 18:48:14 +0300

    added test console project

---
+++ test/test.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class Program
+{
+    private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    static async Task Main(string[] args)
+    {
+
+
+        var client = new HttpClient();
+
+        var response = await client.GetAsync(data_url);
+        var content = response.Content.ReadAsStringAsync().Result;
+
+        Console.WriteLine();
+
+        Console.ReadLine();
+    }
+}
