2e5f5df542a759d1fba82cfc4cb44b19db4ec2e8
sandman633 <sad_is_bad@outlook.com>
2022-07-19 09:57:08 +0300

    added lambda command

--- wpf App/Infrastructure/Commands/Base/Command.cs
+++ wpf App/Infrastructure/Commands/Base/Command.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace wpf_App.Infrastructure.Commands.Base
 {
-    internal class Command
+    internal abstract class Command : ICommand
     {
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+
+        }
+
+        public abstract bool CanExecute(object? parameter);
+
+        public abstract void Execute(object? parameter);
     }
 }
