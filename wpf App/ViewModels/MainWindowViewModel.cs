fc60c47db8595dfd81f11d66b481bd0c8c4cc576
sandman633 <sad_is_bad@outlook.com>
2022-07-19 07:47:03 +0300

    added window title property

---
+++ wpf App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpf_App.ViewModels.Base;
+
+namespace wpf_App.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Window Title
+        private string _title;
+        /// <summary>Window Title</summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        } 
+        #endregion
+
+
+
+    }
+}
